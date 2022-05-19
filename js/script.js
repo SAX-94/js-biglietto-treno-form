a308a994d0814ed3bf0a5ef0b3cc27fb7ad1706b
SAX_94 <matteosacchi94@gmail.com>
2022-05-19 18:42:48 +0200

    imported part of the script from the previous exercise

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// VARIABLES
+
+const pricePerkm = 0.21;
+const minorsDiscount = 0.8;
+const elderDiscount = 0.6;
+const noDiscount = 1;
+let discount;
+
+let age = parseInt(prompt("Quanti anni hai?"));
+let km = parseInt(prompt("Quanti km devi percorrere?"));
+let price = parseFloat(pricePerkm * km);
+
+// CONDITIONS FOR APPLYING THE DISCOUNT
+
+if (age < 18) {
+    discount = minorsDiscount;
+} else if (age > 65) {
+    discount = elderDiscount;
+} else {
+    discount = noDiscount;
+}
+
+// finalPrice VALUE
+let finalPrice = parseFloat(price * discount).toFixed(2);
